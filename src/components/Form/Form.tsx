af107bfa52fd7aa1f6b8517ae240fa5b905e7785
Chantal <chanticou@gmail.com>
2023-03-21 15:38:51 -0300

    Local storage added

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { BsTrash } from "react-icons/bs";
 import "./Form.css";
 interface ITtask {
   id: number;
@@ -10,12 +12,24 @@ export function Form(): JSX.Element {
   const [newtask, setNewTask] = useState<string>("");
   const [allTasks, setAllTasks] = useState<ITtask[]>([]);
   const [idCounter, setIdCounter] = useState<number>(1);
-  const [getTaskDone, setgetTaskDone] = useState<object>({});
-
+  const [updateInput, setUpdateInput] = useState<string>("");
+  const [myTask, setMyTask] = useState<string>("");
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setAllTasks(JSON.parse(storedTasks));
+    }
+  }, []);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    addTask(newtask);
-    setNewTask("");
+    if (!newtask) {
+      console.log("TAREA VACIA");
+    } else {
+      addTask(newtask);
+      setMyTask(newtask);
+      setNewTask("");
+      localStorage.setItem("tasks", JSON.stringify(allTasks));
+    }
   };
 
   const addTask = (name: string) => {
@@ -26,23 +40,31 @@ export function Form(): JSX.Element {
   const handleDelete = (id: number) => {
     const deleteTask = allTasks.filter((el) => el.id !== id);
     setAllTasks(deleteTask);
+
+    localStorage.setItem("tasks", JSON.stringify(deleteTask));
   };
 
   const handleclickCheckbox = (id: number) => {
-    const updatedTasks = allTasks.map((task) => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
+    const uploadTaskDone = allTasks.map((el) => {
+      if (el.id === id) {
+        return { ...el, done: !el.done };
       }
-      return task;
+      return el;
+    });
+    setAllTasks(uploadTaskDone);
+  };
+
+  const handleUpdate = (value: string, id: number) => {
+    const updatedTasks = allTasks.map((el) => {
+      if (el.id === id) {
+        return { ...el, name: value };
+      }
+      return el;
     });
     setAllTasks(updatedTasks);
   };
-
   return (
     <>
-      <button onClick={() => allTasks.map((el) => console.log(el.done))}>
-        ALLTASKS
-      </button>
       <form onSubmit={(e) => handleSubmit(e)}>
         <div className="form">
           <input
@@ -51,27 +73,39 @@ export function Form(): JSX.Element {
             onChange={(e) => setNewTask(e.target.value)}
             type="text"
           />
-          <input type="submit" value="+" />
+
+          <button className="content_submit">
+            <AiOutlinePlusCircle
+              style={{ fontSize: "30px", cursor: "pointer" }}
+            />
+          </button>
         </div>
 
         <div className="form_content_task">
           {allTasks.map((el) => (
             <div className="form_content" key={el.id}>
-              <span className={`form_task_name ${el.done ? "done" : ""}`}>
-                {el.name}
-              </span>
-              <div className="form_task_contentButton_checkbox">
-                <button
-                  className="form_task_delete"
-                  onClick={() => handleDelete(el.id)}
-                >
-                  Delete
-                </button>
+              {
                 <input
                   onClick={(e) => handleclickCheckbox(el.id)}
                   className="form_task_checkbox"
                   type="checkbox"
                 />
+              }
+              <input
+                onChange={(e) => handleUpdate(e.target.value, el.id)}
+                type="text"
+                value={el.name}
+                className={`form_task_name ${el.done ? "done" : ""}`}
+              />
+              {/* <button onClick={() => handleUpdate(el.id)}>actualizar</button> */}
+
+              <div className="form_task_contentButton_checkbox">
+                <button
+                  className="form_task_delete"
+                  onClick={() => handleDelete(el.id)}
+                >
+                  <BsTrash />
+                </button>
               </div>
             </div>
           ))}
